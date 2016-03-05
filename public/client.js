49a86ff90bc75f5f4209141b4ae58cc7d040f775
Michael Wong <michael.brian.wong@gmail.com>
2016-03-05 13:47:16 -0700

    A user can vote

--- public/client.js
+++ public/client.js
@@ -1,7 +1,26 @@
 var socket = io();
-
 var connectionCount = document.getElementById('connection-count');
+var buttons = document.querySelectorAll('#choices button');
+var voteMessage = document.getElementById('vote-message');
+var voteTally = document.getElementById('vote-tally');
 
 socket.on('usersConnected', function (count) {
     connectionCount.innerText = 'Connected Users: ' + count;
 });
+
+socket.on('voteCount', function (votes) {
+    voteTally.innerText =  '  ' + votes['A'];
+    voteTally.innerText += '  ' + votes['B'];
+    voteTally.innerText += '  ' + votes['C'];
+    voteTally.innerText += '  ' + votes['D'];
+});
+
+socket.on('voteMessage', function (vote) {
+    voteMessage.innerText = 'Thanks for voting for ' + vote;
+});
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+        socket.send('voteCast', this.innerText);
+    });
+}
--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const socketIo = require('socket.io');
-
 var port = process.env.PORT || 3000;
 var server = http.createServer(app);
+var votes = {};
 const io = socketIo(server);
+
+function countVotes(votes) {
+    var voteCount = {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+    };
+    for (var vote in votes) {
+        voteCount[votes[vote]]++
+    }
+    return voteCount;
+}
 
 server.listen(port, function () {
     console.log('Listening on port ' + port);
@@ -22,8 +35,18 @@ io.on('connection', function (socket) {
     io.sockets.emit('usersConnected', io.engine.clientsCount);
     socket.emit('statusMessage', 'You have connected.');
 
+    socket.on('message', function (channel, message) {
+        if (channel === 'voteCast') {
+            votes[socket.id] = message;
+            socket.emit('voteMessage', message);
+            socket.emit('voteCount', countVotes(votes));
+        }
+    });
+
     socket.on('disconnect', function () {
         console.log('A user has disconnected.', io.engine.clientsCount);
+        delete votes[socket.id];
+        console.log(votes);
         io.sockets.emit('usersConnected', io.engine.clientsCount);
     });
 });
