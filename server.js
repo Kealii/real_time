6346294e46cc42d712759428c294dda13d94e7c9
Michael Wong <michael.brian.wong@gmail.com>
2016-03-05 13:36:48 -0700

    Adds poll buttons and user count

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const socketIo = require('socket.io');
 
 var port = process.env.PORT || 3000;
 var server = http.createServer(app);
+const io = socketIo(server);
 
 server.listen(port, function () {
     console.log('Listening on port ' + port);
@@ -13,6 +15,17 @@ app.use(express.static('public'));
 
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('A user has connected.', io.engine.clientsCount);
+    io.sockets.emit('usersConnected', io.engine.clientsCount);
+    socket.emit('statusMessage', 'You have connected.');
+
+    socket.on('disconnect', function () {
+        console.log('A user has disconnected.', io.engine.clientsCount);
+        io.sockets.emit('usersConnected', io.engine.clientsCount);
+    });
 });
 
 module.exports = server;
